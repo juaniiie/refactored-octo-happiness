8e8edab64bd7451fa205c65c3c2f32fca5901b48
juaniiie <becjuana@gmail.com>
2017-07-17 22:22:02 -0700

    Works

--- tabs/tabs.js
+++ tabs/tabs.js
@@ -1,11 +1,23 @@
 $(function() {
-    let $list = $('ul');
+    let $list = $('.tab-list');
 
-    //Add click event to ul
-    $list.on('click', function(e) {
-        let $this = $(this);
-        
+    $list.on('click', 'li', function(e) {
+        e.preventDefault();
+        let $tab = $(this);
+        let $link = $tab.find('a').attr('href');
+        let $panel = $($link);
+
+        if (!$tab.hasClass('active')) {
+            deactivateCurrent();
+            $panel.addClass('active');
+            $tab.addClass('active');
+        }
     });
     
-
+    function deactivateCurrent() {
+        let $currentTab = $list.find('li.active');
+        let $currentPanel = $($currentTab.find('a').attr('href'));
+        $currentPanel.removeClass('active');
+        $currentTab.removeClass('active');
+    }
 });
